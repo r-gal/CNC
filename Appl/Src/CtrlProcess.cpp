aaef8c3bcf5c61986193ba691f61069645d2d162
rgal <ryszard.gal@gmail.com>
2025-07-07 23:49:51 +0200

    Add gitignore

--- Appl/Src/CtrlProcess.cpp
+++ Appl/Src/CtrlProcess.cpp
@@ -18,19 +18,8 @@
 
 
 
-void vFunction100msTimerCallback( TimerHandle_t xTimer )
-{
-  //dispActTimSig.Send();
-
-}
-
 CtrlProcess_c::CtrlProcess_c(uint16_t stackSize, uint8_t priority, uint8_t queueSize, HANDLERS_et procId) : process_c(stackSize,priority,queueSize,procId,"CONFIG")
 {
-
- // TimerHandle_t timer = xTimerCreate("",pdMS_TO_TICKS(500),pdTRUE,( void * ) 0,vFunction100msTimerCallback);
- // xTimerStart(timer,0);
-
-  
 
 }
 
@@ -38,7 +27,6 @@ void CtrlProcess_c::main(void)
 {
   RunTime_c::Start();
   timeUnit.Init();
-  //bootUnit.Init(TimeUnit_c::GetHrtc());
   ethernet.Init();
 
   #if DEBUG_PROCESS > 0
@@ -46,11 +34,6 @@ void CtrlProcess_c::main(void)
   #endif
 
   new CNC2_socket_c;
-
-  /* SD card test */
-
-  
-
 
   while(1)
   {
